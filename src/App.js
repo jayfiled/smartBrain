c3fc9bcc1a7f7395d1e65fcceb845084902f56de
Joel James <jayfiled@gmail.com>
2019-02-11 23:16:08 +1100

    build navigation, logo and set styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
+import Logo from './components/Logo/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
-        <ImageLinkForm />
+        <Logo />
+        {/* <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
     );
