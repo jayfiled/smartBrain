3280c20acd3df3fc9a1a6c9278ec7f723c777caf
Joel James <jayfiled@gmail.com>
2019-02-13 22:44:01 +1100

    add clarify api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import clarifai from 'clarifai';
+
+const app = new clarifai.App({
+  apiKey: '5ee068d0215f4be4b3bde048589789ad'
+})
 
 const particlesOptions = {
     particles: {
@@ -26,12 +31,25 @@ class App extends Component {
     }
   }
 
+
+
   onInputChange = (event) => {
     console.log(event.target.value);
   }
 
   onButtonSubmit= () => {
     console.log('clicked');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
