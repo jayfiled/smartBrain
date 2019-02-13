f4492514e9f34c8e600e37a1161930b2829962b3
Joel James <jayfiled@gmail.com>
2019-02-13 22:31:18 +1100

    wire up some event handlers for input and onclick

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit= () => {
+    console.log('clicked');
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +42,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+         onInputChange={this.onInputChange}
+         onButtonSubmit={this.onButtonSubmit} />
        {/*  <FaceRecognition /> */}
       </div>
     );
