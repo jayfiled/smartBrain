9bcf090d5bb343567ba3d44aa16e7b48eb726480
jayfiled <jayfiled@gmail.com>
2019-02-22 23:25:05 +1100

    start work on add box around face

--- src/App.js
+++ src/App.js
@@ -29,8 +29,17 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.output[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
@@ -42,14 +51,8 @@ class App extends Component {
     app.models.predict(
       clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err))
   }
 
   render() {
