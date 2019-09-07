9584879b269fbf2eeb66f522c6994b835ac702cc
Joel James <jayfiled@gmail.com>
2019-09-07 22:46:50 +1000

    finish signin by routing to home if sigin succeeds

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -28,7 +28,11 @@ class Signin extends React.Component {
           })
         })
         .then(response => response.json())
-        this.props.onRouteChange('home');
+        .then(data => {
+            if (data === 'success') {
+                this.props.onRouteChange('home');
+            }
+        })
       }
     
       render() {
